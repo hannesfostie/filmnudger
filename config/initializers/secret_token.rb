fa19adefc570e1b0de895dab4757498713bc66f5
Hannes Fostie <hannes@openminds.be>
2012-10-12 17:00:12 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Filmnudger::Application.config.secret_token = '70343df142157eca32fdd972eccb7e2a823d06f52da3ea2a9074e0c8e4deb07f03f898199b256ee1f782d129d367d9f9a8c3a1644cce8d4115cc86d18e99c6ea'
